ff5bf945ef83e72154c3137eed6c6e2e0d999510
Kairos0ne <kairos0ne@icloud.com>
2022-12-22 22:10:06 +0000

    testing custom inspect protocol

--- lib/open_api_spex/inspect/for_schema.ex
+++ lib/open_api_spex/inspect/for_schema.ex
@@ -1,17 +1,17 @@
-defimpl Inspect, for: OpenApiSpex.Schema do
-  import Inspect.Algebra
+# defimpl Inspect, for: OpenApiSpex.Schema do
+#   import Inspect.Algebra
 
-  # def inspect(parameter, opts) do
-  #   concat(["OpenApiSpex.Schema.new(", Inspect.Map.inspect(parameter, opts), ")"])
-  # end
+#   # def inspect(parameter, opts) do
+#   #   concat(["OpenApiSpex.Schema.new(", Inspect.Map.inspect(parameter, opts), ")"])
+#   # end
 
-  def inspect(schema, opts) do
-    concat([
-      "OpenApiSpex.Schema.new(",
-      inspect(schema.__struct__),
-      ", ",
-      Inspect.Map.inspect(schema, opts),
-      ")"
-    ])
-  end
-end
+#   def inspect(schema, opts) do
+#     concat([
+#       "OpenApiSpex.Schema.new(",
+#       inspect(schema.__struct__),
+#       ", ",
+#       Inspect.Map.inspect(schema, opts),
+#       ")"
+#     ])
+#   end
+# end
